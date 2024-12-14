b23d56a392ebf092514df479c97f4f7aa9383ed2
ITninja04 <info@altiview.us>
2024-12-14 11:38:09 -0600

    updated n8n to latest

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -39,9 +39,9 @@ if [ -n "${NODE_FUNCTION_ALLOW_EXTERNAL}" ]; then
     done
 fi
     
-export N8N_BASIC_AUTH_ACTIVE="$(jq --raw-output '.auth // empty' $CONFIG_PATH)"
-export N8N_BASIC_AUTH_USER="$(jq --raw-output '.auth_username // empty' $CONFIG_PATH)"
-export N8N_BASIC_AUTH_PASSWORD="$(jq --raw-output '.auth_password // empty' $CONFIG_PATH)"
+# export N8N_BASIC_AUTH_ACTIVE="$(jq --raw-output '.auth // empty' $CONFIG_PATH)"
+# export N8N_BASIC_AUTH_USER="$(jq --raw-output '.auth_username // empty' $CONFIG_PATH)"
+# export N8N_BASIC_AUTH_PASSWORD="$(jq --raw-output '.auth_password // empty' $CONFIG_PATH)"
 export GENERIC_TIMEZONE="$(jq --raw-output '.timezone // empty' $CONFIG_PATH)"
 export N8N_PROTOCOL="$(jq --raw-output '.protocol // empty' $CONFIG_PATH)"
 export N8N_SSL_CERT="/ssl/$(jq --raw-output '.certfile // empty' $CONFIG_PATH)"
@@ -49,11 +49,11 @@ export N8N_SSL_KEY="/ssl/$(jq --raw-output '.keyfile // empty' $CONFIG_PATH)"
 export N8N_CMD_LINE="$(jq --raw-output '.cmd_line_args // empty' $CONFIG_PATH)"
 export N8N_USER_FOLDER="${N8N_PATH}"
 
-if [ -z "${N8N_BASIC_AUTH_USER}" ] || [ -z "${N8N_BASIC_AUTH_ACTIVE}" ]; then
-    export N8N_BASIC_AUTH_ACTIVE=false
-    unset N8N_BASIC_AUTH_USER
-    unset N8N_BASIC_AUTH_PASSWORD
-fi
+# if [ -z "${N8N_BASIC_AUTH_USER}" ] || [ -z "${N8N_BASIC_AUTH_ACTIVE}" ]; then
+#     export N8N_BASIC_AUTH_ACTIVE=false
+#     unset N8N_BASIC_AUTH_USER
+#     unset N8N_BASIC_AUTH_PASSWORD
+# fi
 
 ###########
 ## MAIN  ##
